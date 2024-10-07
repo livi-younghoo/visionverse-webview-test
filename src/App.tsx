d02d82ee1dbf128ac467e8d560c2a03991e87f27
younghoo <public.yhkim@gmail.com>
2024-10-07 12:25:03 +0900

    chore: 웹 접근성 테스트를 위한 코드 삽입

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef } from "react";
+import "./App.css";
+import {
+  Route,
+  BrowserRouter as Router,
+  Routes,
+  useSearchParams,
+} from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+const Main = () => {
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const [searchParams] = useSearchParams();
+
+  useEffect(() => {
+    if (buttonRef.current && searchParams.get("isWebviewAccess")) {
+      buttonRef.current.focus();
+    }
+  }, [searchParams]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {Array.from(
+        {
+          length: 20,
+        },
+        (_, idx) => {
+          return (
+            <button key={idx}>{idx} 포커스가 가도 읽히면 안되는 요소</button>
+          );
+        }
+      )}
+      <button ref={buttonRef}>의도적으로 포커스 발생시킨 요소</button>
     </>
-  )
+  );
+};
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
